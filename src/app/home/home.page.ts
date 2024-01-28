c848a259be3f3370688e715464afffea1d2f00ac
Damian Polak <damianpolak.it@gmail.com>
2024-01-28 12:43:25 +0100

    grid

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  squareRoot: number = 9;
+  notesGridSquareRoot: number = 3;
+  isValueMode: boolean = false;
 
-  constructor() {}
+  board: number[][] = [];
+  notesGrid: number[][] = []
+  constructor() {
+    for (let row = 0; row < this.squareRoot; row++) {
+      this.board.push([]);
+      for (let col = 0; col < this.squareRoot; col++) {
+        this.board[row][col] = Math.round(Math.random() * 9);
+      }
+    }
 
+    let count = 1;
+    for (let row = 0; row < this.notesGridSquareRoot; row++) {
+      this.notesGrid.push([]);
+      for (let col = 0; col < this.notesGridSquareRoot; col++) {
+        this.notesGrid[row][col] = count;
+        count++;
+      }
+    }
+
+    this.setBoardSize(this.squareRoot);
+  }
+
+  setBoardSize(size: number): void {
+    document.documentElement.style.setProperty('--boardSize', size.toString());
+  }
 }
