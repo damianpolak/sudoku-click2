acfa4bb4b0810cd0fc30bc1865cb99f1da64cdc4
Damian Polak <damian.polak@tauron.pl>
2024-02-03 18:16:08 +0100

    theme toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  private readonly autoThemeMode: boolean = false;
+  
+  constructor() {
+    this.themeToggler();
+  }
+
+  private themeToggler(): void {
+    if (this.autoThemeMode) {
+      const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+      if (prefersDark.matches) {
+        this.setTheme('dark');
+      } else {
+        this.setTheme('light');
+      }
+    } else {
+      this.setTheme('dark');
+    }
+  }
+
+  private setTheme(theme: 'light' | 'dark'): void {
+    document.body.classList.toggle(theme, true);
+  }
 }
