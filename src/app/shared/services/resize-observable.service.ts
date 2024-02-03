ea6671a072b5771d2fa72b890f82c088af03d115
Damian Polak <damianpolak.it@gmail.com>
2024-02-03 21:31:06 +0100

    new resize observable service

---
+++ src/app/shared/services/resize-observable.service.ts
@@ -0,0 +1,44 @@
+import { filter, map, NextObserver, Observable, Subscriber, throttleTime } from 'rxjs';
+
+export class ResizeObservableService {
+  private resizeObserver: ResizeObserver;
+  private notifiers: NextObserver<ResizeObserverEntry[]>[] = [];
+
+  constructor() {
+    this.resizeObserver = new ResizeObserver((entries: ResizeObserverEntry[]) => {
+      this.notifiers.forEach((obs) => obs.next(entries));
+    });
+  }
+
+  resizeObservable(elem: Element): Observable<ResizeObserverEntry> {
+    this.resizeObserver.observe(elem);
+    const newObserverCandidate = new Observable<ResizeObserverEntry[]>(
+      (subscriber: Subscriber<ResizeObserverEntry[]>) => {
+        this.notifiers.push(subscriber);
+
+        return () => {
+          const idx = this.notifiers.findIndex((val) => val === subscriber);
+          this.notifiers.splice(idx, 1);
+          this.resizeObserver.unobserve(elem);
+        };
+      }
+    );
+
+    return newObserverCandidate.pipe(
+      map((entries) => entries.find((entry) => entry.target === elem)),
+      filter(Boolean),
+      throttleTime(30)
+    );
+  }
+
+  getResizeObservable(elem: Element): Observable<{ width: number; height: number }> {
+    return this.resizeObservable(elem).pipe(
+      map((entry) => {
+        return {
+          width: entry.contentRect.width,
+          height: entry.contentRect.height,
+        };
+      })
+    );
+  }
+}
