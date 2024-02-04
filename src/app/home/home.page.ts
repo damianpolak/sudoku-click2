a242670cd7394d0e0ad0f3595784f42055d72ead
Damian Polak <damianpolak.it@gmail.com>
2024-02-04 11:07:48 +0100

    continue state and home buttons

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { GameStateService } from '../shared/services/game-state.service';
 
 @Component({
   selector: 'app-home',
@@ -7,8 +8,15 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor(private navCtrl: NavController) {}
-  go(): void {
+
+  canContinue = this.gameStateServ.getContinueState$();
+  constructor(private navCtrl: NavController, private gameStateServ: GameStateService) {}
+
+  onContinue(): void {
+    console.log('continue');
+  }
+
+  onNewGame(): void {
     this.navCtrl.navigateForward('game');
   }
 }
--- src/app/shared/services/game-state.service.ts
+++ src/app/shared/services/game-state.service.ts
@@ -5,14 +5,21 @@ import { BehaviorSubject, Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class GameStateService {
+  private readonly continueAvailable = new BehaviorSubject<boolean>(false);
   private readonly pauseState = new BehaviorSubject<boolean>(false);
 
   setPauseState(pause: boolean): void {
+    this.continueAvailable.next(pause);
     this.pauseState.next(pause);
   }
 
   getPauseState$(): Observable<boolean> {
     return this.pauseState.asObservable();
   }
+
+  getContinueState$(): Observable<boolean> {
+    return this.continueAvailable.asObservable();
+  }
+
   constructor() {}
 }
