abe6c25abb6dfc401edb5274abffb79c0de957fe
Damian Polak <damian.polak@tauron.pl>
2024-02-08 15:44:09 +0100

    grid builder

---
+++ src/app/shared/builders/grid.builder.ts
@@ -0,0 +1,21 @@
+
+export class GridBuilder<TGrid> {
+  protected rows!: number;
+  protected cols!: number;
+
+  protected grid: TGrid[][] = [];
+
+  constructor(rows: number, cols: number, value: TGrid) {
+    for (let row = 0; row < rows; row++) {
+      this.grid.push([]);
+      for (let col = 0; col < cols; col++) {
+        this.grid[row][col] = value;
+      }
+    }
+    return this;
+  }
+
+  getGrid(): TGrid[][] {
+    return this.grid;
+  }
+}
