f68526e58d4e251ade9fdb4d0ed9b08fa316b28c
Damian Polak <damianpolak.it@gmail.com>
2024-02-09 09:46:19 +0100

    notes builder

---
+++ src/app/shared/builders/notes.builder.ts
@@ -0,0 +1,70 @@
+type Note = {
+  value: number;
+  active: boolean;
+};
+
+export type Notes = Note[];
+
+export class NotesBuilder {
+  private notes: Set<Note> = new Set();
+  private static readonly MIN_NUMBER = 1;
+  private static readonly MAX_NUMBER = 9;
+  constructor(numbers: number[] = []) {
+    this.create(numbers);
+    return this;
+  }
+
+  protected create(numbers: number[]): void {
+    this.validate(numbers);
+
+    this.notes = new Set();
+    const mode: 'empty' | 'partial' = numbers.length > 0 ? 'partial' : 'empty';
+    for (let i = NotesBuilder.MIN_NUMBER; i <= NotesBuilder.MAX_NUMBER; i++) {
+      this.notes.add({
+        value: i,
+        active: mode === 'empty' ? false : numbers.includes(i) ? true : false,
+      });
+    }
+  }
+
+  update(numbers: number[] = [], options: { active: boolean } = { active: true }): this {
+    this.validate(numbers);
+
+    const entries = Array.from(this.notes).filter((x) => numbers.includes(x.value));
+    entries.forEach((i) => {
+      this.notes.delete(i);
+      this.notes.add({
+        value: i.value,
+        active: options.active,
+      });
+    });
+    this.notes = new Set(Array.from(this.notes).sort((a, b) => a.value - b.value));
+    return this;
+  }
+
+  delete(numbers: number[] = []): this {
+    this.validate(numbers);
+    this.update(numbers, { active: false });
+    return this;
+  }
+
+  get(): Notes {
+    return Array.from(this.notes);
+  }
+
+  getActive(): Notes {
+    return Array.from(this.notes).filter((x) => x.active);
+  }
+
+  getActiveNumbers(): number[] {
+    return Array.from(this.notes)
+      .filter((i) => i.active)
+      .map((i) => i.value);
+  }
+
+  protected validate(numbers: number[]): void | never {
+    if (!numbers.every((x) => x >= NotesBuilder.MIN_NUMBER && x <= NotesBuilder.MAX_NUMBER)) {
+      throw new RangeError('The numbers must be in the range 1 to 9');
+    }
+  }
+}
