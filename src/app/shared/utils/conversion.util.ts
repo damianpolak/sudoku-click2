0f74f35d59a12230826bc5c29fb9b47408fc27d4
Damian Polak <damianpolak.it@gmail.com>
2024-02-20 22:31:00 +0100

    new conversion methods first upper letter

--- src/app/shared/utils/conversion.util.ts
+++ src/app/shared/utils/conversion.util.ts
@@ -6,4 +6,8 @@ export class ConversionUtil {
   static basicOrientationType(orientation: OrientationType): BasicOrientationType {
     return orientation.split('-')[0] as BasicOrientationType;
   }
+
+  static firstUpper(str: string): string {
+    return `${str[0].toUpperCase()}${str.slice(1, str.length).toLowerCase()}`;
+  }
 }
