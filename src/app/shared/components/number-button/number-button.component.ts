8a00b0a9993be5c3782dcfb398da862cc9e031e7
Damian Polak <damian.polak@tauron.pl>
2024-02-22 09:15:26 +0100

    number button animations

--- src/app/shared/components/number-button/number-button.component.ts
+++ src/app/shared/components/number-button/number-button.component.ts
@@ -1,16 +1,34 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostBinding, HostListener, Input, OnInit } from '@angular/core';
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-number-button',
   templateUrl: './number-button.component.html',
   styleUrls: ['./number-button.component.scss'],
 })
-export class NumberButtonComponent  implements OnInit {
+export class NumberButtonComponent implements OnInit {
   @Input() value: string = '';
-  constructor() { }
+
+  @HostListener('click', ['$event'])
+  mouseclick(event: PointerEvent) {
+    this.style = this.sanitizer.bypassSecurityTrustStyle(
+      'transform: scale(1.1); z-index: 10; border: 1px solid black; box-sizing: border-box;'
+    );
+    setTimeout(() => {
+      this.mouseleave(event);
+    }, 300);
+  }
+
+  @HostListener('mouseleave', ['$event'])
+  mouseleave(event: PointerEvent) {
+    this.style = this.sanitizer.bypassSecurityTrustStyle('transform: scale(1); z-index: unset;');
+  }
+
+  @HostBinding('style') style!: SafeStyle;
+
+  constructor(private sanitizer: DomSanitizer) {}
 
   ngOnInit() {
     console.log();
   }
-
 }
