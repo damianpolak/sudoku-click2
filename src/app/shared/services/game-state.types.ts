b9ffcddc81fc64bf809a83c384ac0aa68c09a345
Damian Polak <damianpolak.it@gmail.com>
2024-03-08 19:04:16 +0100

    feat: update GameState type

--- src/app/shared/services/game-state.types.ts
+++ src/app/shared/services/game-state.types.ts
@@ -2,6 +2,7 @@ import { Board } from "src/app/game/board/board.types";
 import { Levels } from "./game-state.service";
 import { HistoryBoard } from "./history.types";
 import { Timestring } from "./timer.types";
+import { Field } from "src/app/game/board/field/field.types";
 
 export type InputMode = 'value' | 'notes';
 
@@ -10,4 +11,6 @@ export type GameState = {
   timestring: Timestring;
   history: HistoryBoard[];
   board: Board;
+  mistakes: number;
+  selectedField: Field;
 }
