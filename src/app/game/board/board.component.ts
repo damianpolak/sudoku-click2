e14086b2c8cdefcd67685e58aa2abc635b42c7ee
Damian Polak <damianpolak.it@gmail.com>
2024-03-14 23:15:06 +0100

    feat: board restart and start animations

--- src/app/game/board/board.component.ts
+++ src/app/game/board/board.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 import { GameLevel, GameStateService } from 'src/app/shared/services/game-state.service';
 import { Address, Field } from './field/field.types';
 import { Board } from './board.types';
 import { SudokuUtil } from 'src/app/shared/utils/sudoku.util';
-import { Subscription } from 'rxjs';
+import { Subscription, tap } from 'rxjs';
 import { BoardService } from './board.service';
+import { Animation, AnimationController } from '@ionic/angular';
+import { Animated } from 'src/app/shared/interfaces/core.interface';
+import { GameStartMode, GameStartType } from 'src/app/shared/services/game-state.types';
 
 @Component({
   selector: 'app-board',
@@ -12,18 +15,32 @@ import { BoardService } from './board.service';
   styleUrls: ['./board.component.scss'],
   providers: [BoardService],
 })
-export class BoardComponent implements OnInit, OnDestroy {
+export class BoardComponent implements Animated, OnInit, OnDestroy, AfterViewInit {
   private _board: Board = [];
   private level!: GameLevel;
   private borderSquares: Array<Record<string, string>> = [];
+  private startBoardAnimation!: Animation;
+  private restartBoardAnimation!: Animation;
 
-  private boardSub$: Subscription = this.boardServ.getBoard$().subscribe((board) => this._board = board);
+  private boardSub$: Subscription = this.boardServ.getBoard$().subscribe((board) => (this._board = board));
+  private readonly gameStartModeSub$: Subscription = this.gameStateServ
+    .getGameStartMode$()
+    .pipe(tap((gameStartMode) => this.animationHandler(gameStartMode)))
+    .subscribe();
+
+
 
   get board() {
     return this._board;
   }
 
-  constructor(private gameStateServ: GameStateService, private boardServ: BoardService) {}
+  constructor(
+    private gameStateServ: GameStateService,
+    private boardServ: BoardService,
+    private animationCtrl: AnimationController,
+    private renderer2: Renderer2,
+    private ref: ElementRef
+  ) {}
 
   ngOnInit() {
     this.loadLevelProperties();
@@ -33,6 +50,11 @@ export class BoardComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.boardSub$.unsubscribe();
+    this.gameStartModeSub$.unsubscribe();
+  }
+
+  ngAfterViewInit(): void {
+    this.setAnimation();
   }
 
   private countBorderSquares(board: Board): Array<Record<string, string>> {
@@ -80,5 +102,61 @@ export class BoardComponent implements OnInit, OnDestroy {
 
   private setBoardSize(size: number): void {
     document.documentElement.style.setProperty('--board-size', size.toString());
+  }
+
+  private animationHandler(gameStartMode: GameStartMode): void {
+    if (gameStartMode.type === GameStartType.RESTART_GAME) {
+      setTimeout(async () => {
+        await this.restartBoardAnimation.play();
+      }, 100);
+    } else if (gameStartMode.type === GameStartType.NEW_GAME || gameStartMode.type === GameStartType.CONTINUE) {
+      this.renderer2.setStyle(this.ref.nativeElement, 'transform', 'scale(0)');
+      setTimeout(async () => {
+        await this.startBoardAnimation.play();
+        this.renderer2.setStyle(this.ref.nativeElement, 'transform', 'scale(1)');
+      }, 300);
+    }
+  }
+
+  setAnimation(): void {
+    // prettier-ignore
+    this.startBoardAnimation = this.animationCtrl
+      .create()
+      .addElement(this.ref.nativeElement)
+      .fill('none')
+      .duration(350)
+      .keyframes([
+        { offset: 0.0, transform: 'scale(0.0) rotate(0.0turn)', filter: 'blur(0px)' },
+        { offset: 0.1, transform: 'scale(0.1) rotate(0.1turn)', filter: 'blur(1.5px)' },
+        { offset: 0.2, transform: 'scale(0.2) rotate(0.2turn)', filter: 'blur(2.5px)' },
+        { offset: 0.3, transform: 'scale(0.3) rotate(0.3turn)', filter: 'blur(3.5px)' },
+        { offset: 0.4, transform: 'scale(0.4) rotate(0.4turn)', filter: 'blur(4.5px)' },
+        { offset: 0.5, transform: 'scale(0.5) rotate(0.5turn)', filter: 'blur(5px)' },
+        { offset: 0.6, transform: 'scale(0.6) rotate(0.6turn)', filter: 'blur(4.5px)' },
+        { offset: 0.7, transform: 'scale(0.7) rotate(0.7turn)', filter: 'blur(3.5px)' },
+        { offset: 0.8, transform: 'scale(0.8) rotate(0.8turn)', filter: 'blur(2.5px)' },
+        { offset: 0.9, transform: 'scale(0.9) rotate(0.9turn)', filter: 'blur(1.5px)' },
+        { offset: 1.0, transform: 'scale(1.0) rotate(1.0turn)', filter: 'blur(0px)' },
+      ]);
+
+    // prettier-ignore
+    this.restartBoardAnimation = this.animationCtrl
+      .create()
+      .addElement(this.ref.nativeElement)
+      .fill('none')
+      .duration(350)
+      .keyframes([
+        { offset: 0.0, transform: 'rotate(0.0turn) scale(1.0)', filter: 'blur(0px)' },
+        { offset: 0.1, transform: 'rotate(0.1turn) scale(0.9)', filter: 'blur(1.5px)' },
+        { offset: 0.2, transform: 'rotate(0.2turn) scale(0.8)', filter: 'blur(2.5px)' },
+        { offset: 0.3, transform: 'rotate(0.3turn) scale(0.7)', filter: 'blur(3.5px)' },
+        { offset: 0.4, transform: 'rotate(0.4turn) scale(0.6)', filter: 'blur(4.5px)' },
+        { offset: 0.5, transform: 'rotate(0.5turn) scale(0.5)', filter: 'blur(5px)' },
+        { offset: 0.6, transform: 'rotate(0.6turn) scale(0.6)', filter: 'blur(4.5px)' },
+        { offset: 0.7, transform: 'rotate(0.7turn) scale(0.7)', filter: 'blur(3.5px)' },
+        { offset: 0.8, transform: 'rotate(0.8turn) scale(0.8)', filter: 'blur(2.5px)' },
+        { offset: 0.9, transform: 'rotate(0.9turn) scale(0.9)', filter: 'blur(1.5px)' },
+        { offset: 1.0, transform: 'rotate(1.0turn) scale(1.0)', filter: 'blur(0px)' },
+      ]);
   }
 }
