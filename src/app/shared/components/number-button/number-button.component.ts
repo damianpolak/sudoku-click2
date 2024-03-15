c39d049cf1acc75310f037717fe138ac468ef669
Damian Polak <damianpolak.it@gmail.com>
2024-03-15 09:28:32 +0100

    fix: change number btn animation to ionic native

--- src/app/shared/components/number-button/number-button.component.ts
+++ src/app/shared/components/number-button/number-button.component.ts
@@ -1,31 +1,49 @@
-import { Component, HostBinding, HostListener, Input, OnInit } from '@angular/core';
-import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
+import { AfterViewInit, Component, ElementRef, HostListener, Input, OnInit } from '@angular/core';
+import { Animated } from '../../interfaces/core.interface';
+import { Animation, AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-number-button',
   templateUrl: './number-button.component.html',
   styleUrls: ['./number-button.component.scss'],
 })
-export class NumberButtonComponent implements OnInit {
+export class NumberButtonComponent implements Animated, OnInit, AfterViewInit {
   @Input() value: string = '';
   @Input() missingValue: string = '';
+  private buttonAnimation!: Animation;
+  animationsEnabled: boolean = true;
+
   @HostListener('click', ['$event'])
   mouseclick(event: PointerEvent) {
-    this.style = this.sanitizer.bypassSecurityTrustStyle('transform: scale(1.1); z-index: 10;');
-    setTimeout(() => {
-      this.mouseleave(event);
-    }, 300);
+    if(this.animationsEnabled) {
+      this.buttonAnimation.play();
+    }
   }
 
-  @HostListener('mouseleave', ['$event'])
-  mouseleave(event: PointerEvent) {
-    this.style = this.sanitizer.bypassSecurityTrustStyle('transform: scale(1); z-index: unset;');
-  }
-
-  @HostBinding('style') style!: SafeStyle;
-  constructor(private sanitizer: DomSanitizer) {}
+  constructor(private ref: ElementRef, private animationCtrl: AnimationController) {}
 
   ngOnInit() {
     console.log();
+  }
+
+  ngAfterViewInit(): void {
+    this.setAnimation();
+  }
+
+  setAnimation(): void {
+    setTimeout(() => {
+      // prettier-ignore
+      this.buttonAnimation = this.animationCtrl
+      .create()
+      .addElement(this.ref.nativeElement)
+      .fill('none')
+      .duration(250)
+      .easing('ease-out')
+      .keyframes([
+        { offset: 0.0, transform: 'scale(1.00)', 'z-index': '10'},
+        { offset: 0.5, transform: 'scale(1.15)', 'z-index': '10'},
+        { offset: 1.0, transform: 'scale(1.00)', 'z-index': '10'},
+      ]);
+    }, 500);
   }
 }
