640e42ac44874643b7142b0aa81853e992293060
Damian Polak <damianpolak.it@gmail.com>
2024-03-15 22:01:02 +0100

    feat: hide background and missing in notes mode

--- src/app/shared/components/number-button/number-button.component.ts
+++ src/app/shared/components/number-button/number-button.component.ts
@@ -1,29 +1,59 @@
-import { AfterViewInit, Component, ElementRef, HostListener, Input, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  Input,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 import { Animated } from '../../interfaces/core.interface';
 import { Animation, AnimationController } from '@ionic/angular';
+import { GameStateService } from '../../services/game-state.service';
+import { InputMode } from '../../services/game-state.types';
+import { Subscription, tap } from 'rxjs';
 
 @Component({
   selector: 'app-number-button',
   templateUrl: './number-button.component.html',
   styleUrls: ['./number-button.component.scss'],
 })
-export class NumberButtonComponent implements Animated, OnInit, AfterViewInit {
+export class NumberButtonComponent implements Animated, OnDestroy, AfterViewInit {
+  private readonly inputModeSub$: Subscription = this.gameStateServ
+    .getInputMode$()
+    .pipe(
+      tap((_) => {
+        this.background = _ === 'notes' ? 'transparent' : '';
+      })
+    )
+    .subscribe((v) => (this._inputMode = v));
   @Input() value: string = '';
   @Input() missingValue: string = '';
   private buttonAnimation!: Animation;
+  private _inputMode!: InputMode;
   animationsEnabled: boolean = true;
 
+  get inputMode(): InputMode {
+    return this._inputMode;
+  }
+
+  @HostBinding('style.background') background!: string;
   @HostListener('click', ['$event'])
   mouseclick(event: PointerEvent) {
-    if(this.animationsEnabled) {
+    if (this.animationsEnabled) {
       this.buttonAnimation.play();
     }
   }
 
-  constructor(private ref: ElementRef, private animationCtrl: AnimationController) {}
+  constructor(
+    private ref: ElementRef,
+    private animationCtrl: AnimationController,
+    private gameStateServ: GameStateService
+  ) {}
 
-  ngOnInit() {
-    console.log();
+  ngOnDestroy(): void {
+    this.inputModeSub$.unsubscribe();
   }
 
   ngAfterViewInit(): void {
