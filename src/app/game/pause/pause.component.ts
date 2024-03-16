41bfd914cad93b0729137f07b6e24331a7c4a7d7
Damian Polak <damianpolak.it@gmail.com>
2024-03-16 23:31:58 +0100

    refactor: extends modal abstract

--- src/app/game/pause/pause.component.ts
+++ src/app/game/pause/pause.component.ts
@@ -1,50 +1,17 @@
-import { AfterViewInit, Component, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
-import { ModalController } from '@ionic/angular';
-import { IonModal } from '@ionic/angular/common';
+import { Component } from '@angular/core';
 import { PauseModalActionType } from './pause.types';
-import { GameStateService } from 'src/app/shared/services/game-state.service';
-import { Subscription } from 'rxjs';
+import { DynamicModalComponent } from 'src/app/shared/abstracts/modal.abstract';
 
 @Component({
   selector: 'app-pause',
   templateUrl: './pause.component.html',
   styleUrls: ['./pause.component.scss'],
 })
-export class PauseComponent implements AfterViewInit, OnDestroy {
-  @Input() isOpen: boolean = false;
-  @Output() actionEvent = new EventEmitter<PauseModalActionType>();
-  @ViewChild('pauseModal') pauseModal!: IonModal;
-
-  private pauseModalWillDismissSub$!: Subscription;
-  private pasueModalDidPresentSub$!: Subscription;
-
+export class PauseComponent extends DynamicModalComponent<PauseModalActionType> {
   buttonsSize: 'small' | 'default' | 'large' = 'default';
 
-  /**
-   * @TODO INTEGRATE WITH OPTIONS
-   */
-  backdropDismissEnabled: boolean = false;
-
-  constructor(private gameStateServ: GameStateService) {}
-
-  ngAfterViewInit(): void {
-    this.pauseModalWillDismissSub$ = this.pauseModal.willDismiss.subscribe((result) => {
-      this.isOpen = false;
-      this.actionEvent.emit(typeof result.detail.data === 'undefined' ? 'DISMISS' : result.detail.data);
-    });
-
-    this.pasueModalDidPresentSub$ = this.pauseModal.didPresent.subscribe(() => {
-      this.isOpen = true;
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.pauseModalWillDismissSub$.unsubscribe();
-    this.pasueModalDidPresentSub$.unsubscribe();
-  }
-
-  async action(action: PauseModalActionType): Promise<void> {
-    await this.pauseModal.dismiss(action);
+  constructor() {
+    super();
   }
 
   async onContinueClick(): Promise<void> {
