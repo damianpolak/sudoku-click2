16aee1b14c793e7aca4df7b7f0726a19668ba15b
Damian Polak <damianpolak.it@gmail.com>
2024-03-16 14:01:53 +0100

    refactor: history service extends service store

--- src/app/shared/services/history.service.ts
+++ src/app/shared/services/history.service.ts
@@ -1,55 +1,26 @@
 import { Injectable } from '@angular/core';
 import { NotesBuilder } from '../builders/notes.builder';
 import { HistoryBoard } from './history.types';
-import { BehaviorSubject, Observable, Subscription, of } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { ServiceStore } from '../abstracts/service-store.abstract';
 
 @Injectable({
   providedIn: 'root',
 })
-export class HistoryService {
-  private _historyBoard: HistoryBoard[] = [];
-  private readonly historyBoard$: BehaviorSubject<HistoryBoard[]> = new BehaviorSubject<HistoryBoard[]>([]);
-  private historyBoardSub$!: Subscription;
-  private get historyBoard() {
-    return this._historyBoard;
-  }
-
-  constructor() {}
-
-  add(history: HistoryBoard[]): void {
-    this.historyBoard$.next([...this._historyBoard, ...history]);
-  }
-
-  clear(): void {
-    this.historyBoard$.next([]);
-  }
-
-  get$(): Observable<HistoryBoard[]> {
-    return this.historyBoard$.asObservable();
-  }
-
-  create(): void {
-    if (!this.historyBoardSub$ || this.historyBoardSub$.closed) {
-      this.historyBoardSub$ = this.historyBoard$.subscribe((v) => {
-        this._historyBoard = v;
-      });
-    }
-  }
-
-  destroy(): void {
-    this._historyBoard = [];
-    this.historyBoardSub$.unsubscribe();
+export class HistoryService extends ServiceStore<HistoryBoard> {
+  constructor() {
+    super();
   }
 
   private getBeforeLast(): HistoryBoard | undefined {
-    if (this.historyBoard.length > 1) {
+    if (this.store.length > 1) {
       return {
-        board: this.historyBoard.slice(-2, -1)[0].board,
-        selectedField: this.historyBoard.slice(-1)[0].selectedField,
+        board: this.store.slice(-2, -1)[0].board,
+        selectedField: this.store.slice(-1)[0].selectedField,
       };
-    } else if (this.historyBoard.length === 1) {
+    } else if (this.store.length === 1) {
       return {
-        board: this.historyBoard[0].board.map((row) => {
+        board: this.store[0].board.map((row) => {
           return row.map((field) => {
             if (!field.isInitialValue) {
               field.isCorrectValue = false;
@@ -59,7 +30,7 @@ export class HistoryService {
             return field;
           });
         }),
-        selectedField: this.historyBoard[0].selectedField,
+        selectedField: this.store[0].selectedField,
       };
     }
     return undefined;
@@ -67,10 +38,10 @@ export class HistoryService {
 
   back(): Observable<HistoryBoard | undefined> {
     const last = this.getBeforeLast();
-    if (this.historyBoard.length > 1) {
-      this.historyBoard$.next(this.historyBoard.splice(0, this.historyBoard.length - 1));
-    } else if (this.historyBoard.length === 1) {
-      this.historyBoard$.next([]);
+    if (this.store.length > 1) {
+      this.emitter$.next(this.store.splice(0, this.store.length - 1));
+    } else if (this.store.length === 1) {
+      this.emitter$.next([]);
     }
     return of(last);
   }
