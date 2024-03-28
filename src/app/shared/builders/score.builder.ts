18c0a9ac38004594ae09ff79591f94e3c950496f
Damian Polak <damianpolak.it@gmail.com>
2024-03-28 19:12:32 +0100

    feat: assign defaults if parameters undefined

--- src/app/shared/builders/score.builder.ts
+++ src/app/shared/builders/score.builder.ts
@@ -4,17 +4,22 @@ export class ScoreBuilder implements Score {
   scored!: boolean;
   score!: number;
 
-  constructor(payload: Score, isCorrectValue: boolean, multiplier: number) {
+  constructor(payload?: Score, isCorrectValue?: boolean, multiplier?: number) {
     this.assignValue(payload, isCorrectValue, multiplier);
   }
 
-  protected assignValue(payload: Score, isCorrectValue: boolean, multiplier: number): this {
-    if (!payload.scored) {
-      this.scored = true;
-      this.score = isCorrectValue ? payload.score + multiplier : payload.score + 0;
+  protected assignValue(payload?: Score, isCorrectValue?: boolean, multiplier?: number): this {
+    if (payload && isCorrectValue && multiplier) {
+      if (!payload.scored) {
+        this.scored = true;
+        this.score = isCorrectValue ? payload.score + multiplier : payload.score + 0;
+      } else {
+        this.scored = true;
+        this.score = payload.score;
+      }
     } else {
-      this.scored = true;
-      this.score = payload.score;
+      this.score = 0;
+      this.scored = false;
     }
     return this;
   }
