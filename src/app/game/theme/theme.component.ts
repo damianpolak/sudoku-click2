aec6fada4b7acaa1a3533e08fd2cbddaf8d7bc5e
Damian Polak <damianpolak.it@gmail.com>
2024-03-31 19:13:37 +0200

    feat: make important changes in theme component

--- src/app/game/theme/theme.component.ts
+++ src/app/game/theme/theme.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DynamicModalComponent } from 'src/app/shared/abstracts/modal.abstract';
 import { ThemeModalActionType } from './theme.types';
+import { ThemeDefinition, ThemeService } from './theme.service';
+import { Observable, Subscription, from, lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-theme',
@@ -8,10 +10,52 @@ import { ThemeModalActionType } from './theme.types';
   styleUrls: ['./theme.component.scss'],
 })
 export class ThemeComponent extends DynamicModalComponent<ThemeModalActionType> implements OnInit {
-  constructor() {
+  override backdropDismissEnabled: boolean = true;
+  get themes2(): Observable<ThemeDefinition[]> {
+    return this.themeServ.get$();
+  }
+
+  private _themes: ThemeDefinition[] = [];
+  private themesSub$: Subscription = this.themeServ.get$().subscribe((v) => {
+    this._themes = v;
+  });
+
+  get themes(): ThemeDefinition[] {
+    return this._themes;
+  }
+
+  constructor(private readonly themeServ: ThemeService) {
     super();
+    this.themeServ.set([
+      { name: 'light', active: true, background: 'var(--ion-theme-light-presentable)' },
+      { name: 'dark', active: false, background: 'var(--ion-theme-dark-presentable)' },
+      { name: 'pastel', active: false, background: 'var(--ion-theme-pastel-presentable)' },
+    ]);
   }
 
   // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
   ngOnInit() {}
+
+  onThemeClick(themeName: string): void {
+    this.themeServ.set(
+      this.themes.map((t) => {
+        return {
+          ...t,
+          active: t.name === themeName,
+        };
+      })
+    );
+    this.themeServ.setTheme(themeName);
+  }
+
+  getThemeStyle(themeName: string): Record<string, string> {
+    const themeDef = this.themes.find((t) => t.name === themeName);
+    return {
+      '--background': themeDef?.background || '',
+      '--border-width': '0.2rem',
+      '--border-style': 'solid',
+      '--border-color': themeDef?.active ? 'var(--ion-color-primary)' : 'transparent',
+      '--box-shadow': 'none',
+    };
+  }
 }
