bfa0f0b1d686b47892cf7a2deab5d90d45a993f3
Damian Polak <damianpolak.it@gmail.com>
2024-04-07 21:57:53 +0200

    feat: base component now got isReady boolean

--- src/app/shared/abstracts/base-component.abstract.ts
+++ src/app/shared/abstracts/base-component.abstract.ts
@@ -1,6 +1,7 @@
 import { BaseClass } from './base-class.abstract';
 
 export abstract class BaseComponent extends BaseClass {
+  protected isReady: boolean = false;
   constructor() {
     super();
   }
