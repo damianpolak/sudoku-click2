13d88195f53350d562e9c07617860346ee37fe9f
Damian Polak <damianpolak.it@gmail.com>
2024-06-23 20:42:00 +0200

    feat: new menu abstract class

---
+++ src/app/shared/abstracts/menu.abstract.ts
@@ -0,0 +1,37 @@
+import { KeyName } from '../interfaces/core.interface';
+import { StorageService } from '../services/storage.service';
+
+interface BaseMenu {
+  save(value: unknown): Promise<void>;
+  load(): Promise<unknown[]>;
+  entityName: string;
+}
+
+export abstract class Menu<T> implements BaseMenu {
+  protected abstract readonly storageKey: KeyName;
+  protected abstract readonly _entityName: string;
+
+  get entityName(): string {
+    return this._entityName.toLowerCase();
+  }
+
+  constructor(protected readonly storageServ: StorageService) {}
+
+  async save(value: T): Promise<void> {
+    try {
+      await this.storageServ.set(this.storageKey, [...(await this.load()), ...[value]]);
+    } catch (e) {
+      console.error(`Cannot save ${this.entityName}`);
+    }
+  }
+
+  async load(): Promise<T[]> {
+    try {
+      const data = await this.storageServ.get(this.storageKey);
+      return (data ? data : []) as T[];
+    } catch (e) {
+      console.error(`An error occured when trying to load ${this.entityName}`);
+      return [];
+    }
+  }
+}
