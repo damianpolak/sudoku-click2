4a324505d4808dc539eb362c92184b33e7c10b1c
Damian Polak <damianpolak.it@gmail.com>
2024-07-01 21:52:09 +0200

    refactor: change name of android app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,11 +2,11 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'sudoku-click2',
+  appName: 'Sudoku Click',
   webDir: 'www',
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: 'https',
+  },
 };
 
 export default config;
