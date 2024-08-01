0b987e0dc08dceb3e08da85495f558734c3dc510
Damian Polak <damianpolak.it@gmail.com>
2024-08-01 23:20:28 +0200

    feat: update build version script

--- build-version.ts
+++ build-version.ts
@@ -1,5 +1,6 @@
 import { writeFileSync } from 'fs';
 import { join } from 'path';
+import { Build, BuildVersion } from 'src/app/shared/interfaces/core.interface';
 
 const BUILD_VERSION_PATH = join(__dirname, 'src/build-version.json');
 
@@ -15,9 +16,10 @@ try {
   currentIteration = getTimestampBasedVersion();
 }
 
-const buildVersion = {
+const buildVersion: Build = {
+  appVersion: require('./package.json')?.version || 'no info',
   currentIteration: currentIteration + 1,
-  buildVersion: `${require('./package.json')?.version}-build.${currentIteration}`,
+  buildVersion: (`${require('./package.json')?.version}-build.${currentIteration}` as BuildVersion) || 'no info',
 };
 
 writeFileSync(BUILD_VERSION_PATH, JSON.stringify(buildVersion, null, 2));
