9093377fca6419f3a3a40ef611cab4d0e7a4196f
Damian Polak <damianpolak.it@gmail.com>
2024-08-01 23:20:10 +0200

    feat: build types

--- src/app/shared/interfaces/core.interface.ts
+++ src/app/shared/interfaces/core.interface.ts
@@ -4,3 +4,11 @@ export interface Animated {
 }
 
 export type KeyName = `SUDOKU_${string}`;
+
+export type BuildVersion = `${number}.${number}.${number}-build.${number}.${number}`;
+
+export type Build = {
+  appVersion: string | 'no info';
+  currentIteration: number;
+  buildVersion: BuildVersion | 'no info';
+};
